86fdce8acc57e5bd4cb4971b590cb7fb676c7157
Mauro <Mauro.styleman@student.kdg.be>
2022-07-27 20:15:04 +0200

    eerste versie UI

--- RPS.js
+++ RPS.js
@@ -1,56 +1,57 @@
 const computerOptions = ["Rock", "Paper", "Scissors"];
-let playerPoints = 0
-let computerPoints = 0
-let tiePoints = 0
+let playerPoints = 0;
+let computerPoints = 0;
+let tiePoints = 0;
 
 function getComputerChoice() {
-    return computerOptions[Math.floor(Math.random() * computerOptions.length)]
+    return computerOptions[Math.floor(Math.random() * computerOptions.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    const score = document.querySelector('#Result')
 
     if ((playerSelection === "rock" && computerSelection === "scissors")
         || (playerSelection === "paper" && computerSelection === "rock")
         || (playerSelection === "scissors" && computerSelection === "paper")) {
 
-        console.log("you win this round")
-        playerPoints++
+
+        score.textContent = "you win this round"
+        playerPoints++;
+        const points = document.querySelector('#P_Win')
+        points.textContent = ` Player Score:${playerPoints}`
     } else if (playerSelection === computerSelection) {
-        console.log("draw")
-        tiePoints++
+        score.textContent = "draw"
+        tiePoints++;
+        const points = document.querySelector('#Draw')
+        points.textContent = `Total Draws:${tiePoints}`
 
     } else {
-        console.log("you lost this round")
-        computerPoints++
+        computerPoints++;
+        score.textContent = "you lost this round"
+        const points = document.querySelector('#C_Win')
+        points.textContent = `ComputerScore:${computerPoints}`
     }
-    return playerSelection && computerSelection
-}
-function game(){
-    let i = 0
-    while (i < 5){
-        const playerSelection = prompt("choice rock paper scissor")
-        const computerSelection = getComputerChoice();
-        console.log("round", i+1)
-        console.log("computer choice:",computerSelection)
-        console.log("player choice:",playerSelection)
-        playRound(playerSelection,computerSelection)
-        console.log("player score", playerPoints)
-        console.log("computer score", computerPoints)
-        console.log("draw score", tiePoints)
-        i++
+    if (playerPoints === 5 || computerPoints === 5) {
+        document.querySelector('#R_Button').disabled = true
+        document.querySelector('#P_Button').disabled = true
+        document.querySelector('#S_Button').disabled = true
+        if (playerPoints > computerPoints) {
+            score.textContent = "Player won this game"
+        } else {
+            score.textContent = "Computer won this game"
+        }
     }
-    if (playerPoints > computerPoints) {
-        console.log("you won the game")
-    }
-    else if (playerPoints < computerPoints) {
-        console.log("you lost the game")
-    }
-    else {
-        console.log("it's a draw")
-    }
+    return playerSelection && computerSelection;
 }
 
-game()
+function game() {
+    document.querySelector('#R_Button').addEventListener('click', () => playRound('rock', getComputerChoice()));
+    document.querySelector('#P_Button').addEventListener('click', () => playRound('paper', getComputerChoice()));
+    document.querySelector('#S_Button').addEventListener('click', () => playRound('scissors', getComputerChoice()));
+    document.querySelector('#Reset_Button').addEventListener('click', () => location.reload());
 
+}
+
+game();
